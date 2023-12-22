349c609b8857001d4add5a206b760106d4ffcd97
totruong <totruong591@gmail.com>
2023-12-22 22:09:51 +0700

    full code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import DetailSong from './component/DetailSong';
 import ListSong from './component/ListSong';
 import Navbar from './component/Navbar';
-
+import DataSongs from './data/songs.json';
+import { Songs } from './Context';
+import Playing from './component/Playing';
+import { useState } from 'react';
 function App() {
+  const [song, setSong] = useState(DataSongs[0])
+  const handleSetSong = (idsong) => {
+    const song = DataSongs.find(song => song.id === idsong)
+    if (!song)
+      setSong(DataSongs[0])
+    else
+      setSong(song);
+  }
   return (
     <div className="App ">
-      <Navbar />
-      <div className='grid grid-cols-3 bg-slate-700 h-screen-navbar-player'>
-        {/* span 1 */}
-        <DetailSong />
-        {/* span 2 */}
-        <ListSong />
-      </div>
+      <Songs.Provider value={{ DataSongs, song, handleSetSong }}>
+        <Navbar />
+        <div className='grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden'>
+          {/* span 1 */}
+          <DetailSong />
+          {/* span 2 */}
+          <ListSong />
+        </div>
+        <Playing />
+      </Songs.Provider>
+
     </div>
   );
 }
